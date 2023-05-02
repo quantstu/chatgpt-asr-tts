493156dea5f6c02459e9ce6efc943de621768209
quantstu <quantstudy@outlook.com>
2023-05-02 16:22:55 +0800

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
   return (
     <>
       <IconRobot size="2rem" stroke={1.5} className="fill-teal-600" />
-      <div className="tracking-wider font-bold">{APP_NAME}</div>
+      <div className="tracking-wider font-bold"></div>
     </>
   );
 }
